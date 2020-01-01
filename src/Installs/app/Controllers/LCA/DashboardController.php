a3eedf1b458350fea02b422d477051968bdb5cf8
Vignesh subash chandran <vignesh-subash@users.noreply.github.com>
2020-01-01 15:54:25 +0530

    Controller update
    Controller update

---
+++ src/Installs/app/Controllers/LCA/DashboardController.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Controller genrated using LaraCRMAdmin
+ * Help: http://laracrm.com
+ */
+
+namespace App\Http\Controllers\LA;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests;
+use Illuminate\Http\Request;
+
+/**
+ * Class DashboardController
+ * @package App\Http\Controllers
+ */
+class DashboardController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        return view('lca.dashboard');
+    }
+}
